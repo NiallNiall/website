5dc466c2bf58ac22231ed260a2223f45f87d286a
niallniall <niall.henn@gmail.com>
2016-09-21 11:27:52 +0100

    video page updates

--- scriptsnh/video.js
+++ scriptsnh/video.js
@@ -1,33 +1,35 @@
 var iframe = document.querySelector('iframe');
-var player = new Vimeo.Player(iframe);
+if (iframe) {
+  var player = new Vimeo.Player(iframe);
 
-var videoPlay = document.getElementById('fs-video');
-if (videoPlay) {
-  var $videoContainer = $('#fs-video-container');
-  var videoClose = $videoContainer.find('.btn-close')[0];
+  var videoPlay = document.getElementById('fs-video');
+  if (videoPlay) {
+    var $videoContainer = $('#fs-video-container');
+    var videoClose = $videoContainer.find('.btn-close')[0];
 
-  videoClose.addEventListener('click', function(e) {
-    videoStop()
-  });
+    videoClose.addEventListener('click', function(e) {
+      videoStop()
+    });
 
-  $(document).keyup(function(e) {
-    if (e.keyCode == 27) { videoStop() }
-  });
+    $(document).keyup(function(e) {
+      if (e.keyCode == 27) { videoStop() }
+    });
 
-  var videoStop = function() {
-    $videoContainer.fadeOut(350, function() {
-      player.pause();
+    var videoStop = function() {
+      $videoContainer.fadeOut(350, function() {
+        player.pause();
+      });
+    }
+
+    videoPlay.addEventListener('click', function(e) {
+
+      player.play();
+
+      $('#fs-video-container').fadeIn(350, function() {
+        $(this).css('display', 'flex');
+      });
     });
   }
-
-  videoPlay.addEventListener('click', function(e) {
-
-    player.play();
-
-    $('#fs-video-container').fadeIn(350, function() {
-      $(this).css('display', 'flex');
-    });
-  });
 }
 
 
